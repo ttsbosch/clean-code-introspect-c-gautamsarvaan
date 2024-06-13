fa6d01bc1531b53fb2cd0719b26ce0d4e2ea7acd
gautamsarvaan <gautamsarvaan@users.noreply.github.com>
2024-06-13 16:21:00 +0530

    Create StringHelper.h

---
+++ StringHelper.h
@@ -0,0 +1,30 @@
+char** SplitString(const char* inputString, char delimiter) {
+    int delimiterCount = 0;
+    const char* ptr = inputString;
+    while (*ptr != '\0') {
+        if (*ptr++ == delimiterCount) {
+            delimiterCount++;
+        }
+    }
+
+    char** tokens = (char**)malloc(sizeof(char*) * (count + 2));
+    int row = 0;
+    ptr = str;
+    char* token = (char*)malloc(strlen(str) + 1);
+    int column = 0;
+    while (*ptr != '\0') {
+        if (*ptr == delimiter) {
+            token[column] = '\0';
+            tokens[row++] = strdup(token);
+            column = 0;
+        } else {
+            token[column++] = *ptr;
+        }
+        ptr++;
+    }
+    token[column] = '\0';
+    tokens[row++] = strdup(token);
+    tokens[row] = NULL;
+    free(token);
+    return tokens;
+}
