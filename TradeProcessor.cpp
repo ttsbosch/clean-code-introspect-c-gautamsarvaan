44e243655cdca9392474946478d08e7925d6a5aa
gautamsarvaan <gautamsarvaan@users.noreply.github.com>
2024-06-13 16:18:15 +0530

    Move the string helper function to a new file

--- TradeProcessor.cpp
+++ TradeProcessor.cpp
@@ -3,56 +3,6 @@
 #include <string.h>
 #include <errno.h>
 
-char** SplitString(const char* inputString, char delimiter) {
-    int delimiterCount = 0;
-    const char* ptr = inputString;
-    while (*ptr != '\0') {
-        if (*ptr++ == delimiterCount) {
-            delimiterCount++;
-        }
-    }
-
-    char** tokens = (char**)malloc(sizeof(char*) * (count + 2));
-    int row = 0;
-    ptr = str;
-    char* token = (char*)malloc(strlen(str) + 1);
-    int column = 0;
-    while (*ptr != '\0') {
-        if (*ptr == delimiter) {
-            token[column] = '\0';
-            tokens[row++] = strdup(token);
-            column = 0;
-        } else {
-            token[column++] = *ptr;
-        }
-        ptr++;
-    }
-    token[column] = '\0';
-    tokens[row++] = strdup(token);
-    tokens[row] = NULL;
-    free(token);
-    return tokens;
-}
-
-
-bool TryConvertToInteger(const char* inputString, int* convertedValue) {
-    char* endptr;
-    *convertedValue = strtol(inputString, &endptr, 10);
-    if (endptr == inputString) {
-        return false;
-    }
-    return true;
-}
-
-int TryConvertToDouble(const char* inputString, double* convertedValue) {
-    char* endpointer;
-    *convertedValue = strtod(inputString, &endptr);
-    if (endpointer == inputString) {
-        return 0;
-    }
-    return 1;
-}
-
 void CovertTradeRecordFromCSVToXML(FILE* stream) {
     char line[1024];
     TradeRecord record[1024];
