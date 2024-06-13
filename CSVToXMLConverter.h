55055d1f4b5a43a800048a5d4fc75b381c5bcf31
gautamsarvaan <gautamsarvaan@users.noreply.github.com>
2024-06-13 16:27:45 +0530

    Create CSVToXMLConverter.h

---
+++ CSVToXMLConverter.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#incluce "StringHelper.h"
+#incluce "converter.h"
+#include "TradeRecord.h"
+
+void CovertTradeRecordFromCSVToXML(FILE* stream);
